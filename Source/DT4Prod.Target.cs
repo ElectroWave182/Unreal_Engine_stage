b6a11b30a792cfa4083baab5c479b514f146791b
Enzo C <pl26.8000@gmail.com>
2026-07-23 10:35:00 +0200

    Correction des warnings au lancement
    - "IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;" dans les fichiers Target.cs
    - Restauration des fichiers manquants de Plugins/LCC4Unreal/Source/ThirdParty

--- Source/DT4Prod.Target.cs
+++ Source/DT4Prod.Target.cs
@@ -9,6 +9,7 @@ public class DT4ProdTarget : TargetRules
 	{
 		Type = TargetType.Game;
 		DefaultBuildSettings = BuildSettingsVersion.V6;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
 
 		ExtraModuleNames.AddRange( new string[] { "DT4Prod" } );
 	}
